0d54427d9a7fd9f1697c4c4f18100f847694e5cf
tonirp <arodriguez@makenai.es>
2018-03-01 16:06:06 +0100

    final nuevo codigo

--- skills/clonenew.js
+++ skills/clonenew.js
@@ -6,6 +6,7 @@ module.exports = function(controller) {
         var email = message.user;
         var CiscoSpark = require('node-ciscospark');
         var async = require('async');
+        var spark = new CiscoSpark(process.env.SPARK_TOKEN);
 
         var spaceSeperator = " ";
         var comaSeperator = ",";
